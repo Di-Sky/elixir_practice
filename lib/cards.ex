c8f35332a1039c59f97cea8bb8c74ea1cc6c1006
Oleksii <akter.cs@gmail.com>
2018-05-10 08:42:58 +0300

    init (save to file implemented)

---
+++ lib/cards.ex
@@ -0,0 +1,41 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello
+      :world
+
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, path_to_file) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(path_to_file, binary)
+  end
+end
